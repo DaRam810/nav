949ecfe3182a3fd1ca9be910c1ec5d268be97f7e
DaRam810 <148483333+DaRam810@users.noreply.github.com>
2024-01-10 14:31:41 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -7,10 +7,11 @@ hamburgerEl.onclick = function () {
 	hamburgerEl.classList.toggle("hamburger--open");
 };
 
+/*
 navEl.addEventListener("click", () => {
 	navEl.classList.remove("nav--open");
 	hamburgerEl.classList.remove("hamburger--open");
-});
+});*/
 
 document.onclick = function (e) {
 	if (!navEl.contains(e.target) && !hamburgerEl.contains(e.target)) {
@@ -18,10 +19,4 @@ document.onclick = function (e) {
 		hamburgerEl.classList.remove("hamburger--open");
 	}
 };
-
-function hasTouch() {
-  return 'ontouchstart' in document.documentElement
-         || navigator.maxTouchPoints > 0
-         || navigator.msMaxTouchPoints > 0;
-}
 
